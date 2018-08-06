ac501e07129547145ab8c82324f1813c17d86100
npacariz <nikolapacariz@gmail.com>
2018-08-06 20:10:02 +0200

    Add for loop

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@ export default {
   components: {
     HelloWorld,
     ContactList
-  }
+  },
+ 
 }
 </script>
 
--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
       <h1>Contact List</h1>
-  
+
+      <table border="1">
+        <thead>
+          <th>Name</th>
+          <th>Email</th>
+        </thead>
+        <tbody>
+          <tr v-for="(contact, index) in contacts" :key='index'>
+            <td>
+                {{ contact.name }}
+            </td>
+            <td>
+                {{ contact.email }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ContactList',
- 
+
+  data() {
+    return {
+      contacts: [
+        {name: "John", email: 'john@gmail.com'},
+        {name: "Jack", email: 'jack@gmail.com'},
+        {name: "Sussan", email: 'Sussan@gmail.com'},
+        {name: "Jasmin", email: 'Jasmin@gmail.com'},
+      ]
+    }
+  }
 }
 </script>
+
+<style>
+
+table {
+  margin: 0 auto;
+}
+
+
+</style>
+
